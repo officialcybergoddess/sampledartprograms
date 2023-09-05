22d7a17612d32c03a046facd0902290be5bfe3b3
Cyber Goddess <davisnimisha2023@gmail.com>
2023-09-05 10:30:39 -0700

    Sample

---
+++ bin/classexample.dart
@@ -0,0 +1,27 @@
+class MyData{
+  String name="Cyber Goddess";
+  int age=38;
+  double? mark;//null aware operator
+  String email="cybergoddessofficial@gmail.com";
+  int phone=8129426863;
+  static String college =" VigilantByte";
+}
+
+void main(){
+  //object creation
+  MyData stud1=MyData();
+  print("Student Details");
+  print("name=${stud1.name}");//calling instance variable
+  print("age=${stud1.age}");
+  print("mark=${stud1.mark=20}");//initializing value
+  print("college=${MyData.college}");//calling static variable
+
+  MyData stud2=MyData();
+  print("Student Details");
+  print("name=${stud2.name}");//calling instance variable
+  print("age=${stud2.age}");
+  print("mark=${stud2.mark}");//this will be null
+  print("college=${MyData.college}");//calling static variable
+
+
+}
