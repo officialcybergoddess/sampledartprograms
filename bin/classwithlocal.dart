8acc90496d70e354f03947ac09bcc84260820a57
Cyber Goddess <davisnimisha2023@gmail.com>
2023-09-05 10:31:51 -0700

    Day 3

---
+++ bin/classwithlocal.dart
@@ -0,0 +1,21 @@
+class LocalVar{
+
+  void add(){
+    int a=10;
+    int b=20;
+    int c=a+b;
+    print("Add: $c");
+  }
+  void sub(){
+    int a=90;
+    int b=80;
+    int c=a-b;
+    print("sub: $c");
+  }
+}
+void main()
+{
+  LocalVar obj1=LocalVar();
+  obj1.add();
+  obj1.sub();
+}
---
+++ bin/localvariable.dart
@@ -0,0 +1,14 @@
+void main() //builtin function
+{
+  String data = "Welcome"; //value initlaizing
+  print("Hello Cyber goddess"); //print using builtin function
+  print("$data");
+  display();//calling function
+
+}
+
+void display() //user defined function or default function
+{
+  String name="Subscribe my channel";
+  print("$name");
+}
